50e50da6231590f7698d85fcb11531e7663eb5c5
susmitha-j <jejigarisusmitha@gmail.com>
2024-07-01 22:14:53 -0500

    Skills section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ import * as AOS from 'aos';
 export class AppComponent {
   title = 'frontend-portfolio';
   ngOnInit() {
-    AOS.init();
+    AOS.init({
+      duration: 1000, // Animation duration
+      easing: 'ease-in-out', // Easing function
+      once: false, // Whether animation should happen only once
+      mirror: false // Whether elements should animate out while scrolling past them
+    });
   } 
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
+  skills : any;
+  ngOnInit() {
 
+
+   this.skills = [
+      { id: '1', skill: 'Spring Boot', progress: '85%' },
+      { id: '2', skill: 'Angular', progress: '85%' },
+      { id: '3', skill: 'SQL', progress: '80%' },
+      { id: '4', skill: 'Java', progress: '75%' },
+      { id: '5', skill: 'AWS', progress: '75%' },
+      { id: '6', skill: 'Kafka', progress: '70%' }
+    ];
+    
+  }
 }
