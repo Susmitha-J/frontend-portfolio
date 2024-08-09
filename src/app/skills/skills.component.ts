4e981b5bdb83d76702b4aba160edf07d1dd32d7a
susmitha-j <jejigarisusmitha@gmail.com>
2024-08-08 21:29:51 -0500

    Changes added

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -14,9 +14,15 @@ export class SkillsComponent {
       { id: '1', skill: 'Spring Boot', progress: '85%' },
       { id: '2', skill: 'Angular', progress: '85%' },
       { id: '3', skill: 'SQL', progress: '80%' },
-      { id: '4', skill: 'Java', progress: '75%' },
-      { id: '5', skill: 'AWS', progress: '75%' },
-      { id: '6', skill: 'Kafka', progress: '70%' }
+      { id: '4', skill: 'Java', progress: '70%' },
+      { id: '5', skill: 'Typescript', progress: '90%' },
+      { id: '6', skill: 'Bootstrap', progress: '80%' },
+      { id: '7', skill: 'Kafka', progress: '75%' },
+      { id: '8', skill: 'AWS', progress: '75%' },
+      { id: '9', skill: 'Machine Learning', progress: '75%' },
+      { id: '10', skill: 'Python', progress: '75%' },
+      { id: '11', skill: 'Flask', progress: '75%' },
+
     ];
     
   }
